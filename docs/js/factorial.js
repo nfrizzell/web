86e2ad756264fc210ef4357dc6e2c8c3c6be4665
nfrizzell <54083373+nfrizzell@users.noreply.github.com>
2022-08-18 19:36:15 -0400

    factorial fix & some other stuff

--- docs/js/factorial.js
+++ docs/js/factorial.js
@@ -3,7 +3,6 @@ const resultField = document.getElementById("result-field");
 
 function factorial() {
     num = parseInt(numberField.value);
-
     let result = 1;
     for (i = 1; i <= num; i++) {
         result *= i;
